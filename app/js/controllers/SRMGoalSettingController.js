8716611cd4112898a243587d8c06ab8de090219a
cbitstech <markbegale@CBITss-MacBook-Pro.local>
2016-07-15 00:15:16 -0500

    big arms update

--- app/js/controllers/SRMGoalSettingController.js
+++ app/js/controllers/SRMGoalSettingController.js
@@ -7,46 +7,49 @@
   	this.goals = SRMGoalsContent.Goals;
 
     this.currentGoalIndex = 0;
-    this.currentGoalTaskCompleted = false;
-    this.currentGoalPageFinished = false;
-    this.currentGoal = this.goals[this.currentGoalIndex];
-    this.currentHighestUnlockedGoalStage = 5;
-    this.currentStageIndex = $routeParams.goal_stage_index || this.currentHighestUnlockedGoalStage;
-    this.currentStage = this.stages[this.currentStageIndex];
     this.currentStrategyIndex = 0;
+
+
     this.nextGoalIndex = null;
-    this.nextGoalSaved = false;
+    this.currentHighestUnlockedGoalStage = 5;
+    this.panelsVisible = [true,false,false];
+
+    this.showPanel = function(panelIndex){
+      for(var i = 0; i < this.panelsVisible.length; i++){
+          this.panelsVisible[i] = false;
+      }
+      this.panelsVisible[panelIndex] = true;
+    }
+
+    this.init = function(){
+      if (this.currentStageIndex() === 0){
+        this.panelsVisible = [false,true,false];
+      }
+    }
+
+    this.currentStageIndex = function(){
+        return parseInt($routeParams.goal_stage_index) > -1 ? parseInt(parseInt($routeParams.goal_stage_index)) : this.currentHighestUnlockedGoalStage;
+    }
+
+    this.currentStage = this.stages[this.currentStageIndex()];
+    this.currentGoal = this.goals[this.currentGoalIndex];
 
     this.goalSuccess = function(){
         return this.currentGoalTaskCompleted == 'Yes'
     }
-
-    this.goalSuccessHTML = $sce.trustAsHtml(this.currentStage.goalSuccessHTML.success);
 
     this.goalFailure = function(){
         return this.currentGoalTaskCompleted == 'No'
     }
 
     this.goalFailureHTML = $sce.trustAsHtml(this.currentStage.goalSuccessHTML.failure);
-
-
-    this.goalCompletionRequired = function(){
-        return this.currentStageIndex > 0 && !this.currentGoalPageFinished
-    }
-
-    this.finishGoalTracking = function(){
-        this.currentGoalPageFinished = true;
-    }
-
-    this.goalCompleted = function(){
-        return !this.nextGoalSaved && !this.goalCompletionRequired()
-    }
+    this.goalSuccessHTML = $sce.trustAsHtml(this.currentStage.goalSuccessHTML.success);
 
     this.availableGoals = function(){
 
       var availableGoals = [];
        for(var i = 0; i <this.goals.length; i++){
-          if( this.goals[i].availability.indexOf(this.currentStageIndex) > -1){
+          if( this.goals[i].availability.indexOf(this.currentStageIndex()) > -1){
           availableGoals.push(this.goals[i]);
         }
 
@@ -55,17 +58,14 @@
        return availableGoals
     }
 
-    this.saveNextGoal = function(){
-        this.nextGoalSaved = true;
-    }
-
     this.nextGoal = function(){
+      debugger;
         return this.goals[this.nextGoalIndex];
     }
 
     this.strategiesHTML = function(){ 
       var strategiesHTML = '';
-      if (this.nextGoal() != undefined){
+      if (this.nextGoal() != undefined){debugger;
       strategiesHTML = this.nextGoal().strategiesHTML[this.currentStrategyIndex];
       }
       return $sce.trustAsHtml(strategiesHTML)
