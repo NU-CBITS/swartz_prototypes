c6d8157169e8bbb2683489906191bc02fa8b9a9c
cbitstech <markbegale@CBITss-MacBook-Pro.local>
2016-07-15 07:11:12 -0500

    add number to lesson current page

--- app/js/controllers/LessonsCurrentController.js
+++ app/js/controllers/LessonsCurrentController.js
@@ -4,6 +4,7 @@
   function LessonsCurrentController($location, $routeParams, LessonPlans) {
     this.lessonPlan = LessonPlans;
     this.currentLessonPlan = $routeParams.lessons_index || 2;
+    this.currentLessonCounter = this.currentLessonPlan + 1;
   }
 
   angular.module('cbitsPrototype.controllers')
