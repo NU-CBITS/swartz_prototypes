d81b7228d0f55460a7d1e8cb0ac31f219216db70
Caraway <m-begale@northwestern.edu>
2016-07-29 08:59:42 -0700

    I statements

--- app/js/controllers/IStatementsController.js
+++ app/js/controllers/IStatementsController.js
@@ -3,6 +3,14 @@
 
   function IStatementsController() {
 
+  	this.panelVisible = [true,false,false,false];
+
+    this.showPanel = function(panelIndex){
+    	for(var i = 0; i < this.panelVisible.length; i++){
+    		this.panelVisible[i] = false;
+    	}
+    	this.panelVisible[panelIndex] = true;
+    }
   }
 
   angular.module('cbitsPrototype.controllers')
