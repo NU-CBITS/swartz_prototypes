c0fb670677456eed1aa6860833ec478e2ff1e5ee
Caraway <m-begale@northwestern.edu>
2016-07-29 13:50:54 -0500

    disagreements / warning signs / relapse prevention plan

--- app/js/controllers/DisagreementsController.js
+++ app/js/controllers/DisagreementsController.js
@@ -15,6 +15,18 @@
     	}
     	this.panelVisible[panelIndex] = true;
     }
+
+    this.videoURL = 'https://www.youtube.com/watch?v=kjRAWql2A3E';
+
+    this.problems = [
+    {title:'Assuming that the other person knows what you’re thinking but not saying it', answer:0, answers: [' Great.  You noticed that Paulette assumed that Claude knew that she had had a hard day with the kids and needed support from him, but didn’t say it.',' It seems that  Paulette assumed that Claude knew that she had had a hard day with the kids and needed support from him, but didn’t say it.']},
+    {title:'Failing to take the other person’s perspective ', answer:0, answers: [' Great.  You noticed that neither Paulette nor Claude was able to put themselves in their partner’s shoes.',' It seems that  neither Paulette nor Claude was able to put themselves in their partner’s shoes.']},
+    {title:'Giving in without sticking up for yourself ', answer:1, answers: [' It seems that neither Paulette nor Claude gave in, even if they weren’t able to effectively argue their position.',' Yes, neither Paulette nor Claude gave in, even if they weren’t able to effectively argue their position. .']},
+    {title:'Not using “I” statements  ', answer:0, answers: ['Yes, neither Paulette nor Claude used “I” statments.',' Neither Paulette nor Claude used “I” statements .']},
+    {title:'Name calling ', answer:0, answers: [' Yes, Paulette called Claude a “bastard.”',' Paulette called Claude a “bastard.”']},
+    {title:'Not being direct about what they want/need ', answer:0, answers: ['Yes.  Neither Paulette nor Claude communicated clearly about what they wanted in this situation.',' Neither Paulette nor Claude communicated clearly about what they wanted in this situation.']},
+    {title:'Being provocative ', answer:0, answers: [' Yes, both Paulette and Claude were sarcastic and said things to get a rise out of the other person. ',' Both Paulette and Claude were sarcastic and said things to get a rise out of the other person.']}
+    ];
   }
 
   angular.module('cbitsPrototype.controllers')
--- app/js/controllers/EarlyWarningSignsController.js
+++ app/js/controllers/EarlyWarningSignsController.js
@@ -14,6 +14,17 @@
     this.notLastPanel = function(){
         return !this.panelVisible[this.panelVisible.length-1]
     }
+
+    this.content = [
+      {title:'Sleep',list:['Sleeping more','Sleeping less','Broken sleep','Other (free text to specify)']},
+      {title:'Appetite',list:['Eating more','Eating less','Other (free text to specify)']},
+      {title:'Energy',list:['More energy','Less energy','Other (free text to specify)']},
+      {title:'Consultant',list:['Scattered attention','Poor focus','Overly focused on one topic','Other (free text to specify)']},
+      {title:'Interest in activities',list:['Increased interest in activities','Increased interest in activities that I would not normally do','Decreased interest in activities','Decreased interest in any activities, even ones I normally enjoy','Other (free text to specify)']},
+      {title:'Interest in sex',list:['Higher interest in sex','Lower interest in sex','Other (free text to specify)']},
+      {title:'Ability to make decisions',list:['Increased ability to make decisions','Making reckless or impulsive decisions','Decreased ability to make decisions','Other (free text to specify)']},
+      {title:'Level of worry or anxiety',list:['Higher levels of worry/anxiety','Lower levels of worry/anxiety','Other (free text to specify)']}];
+
   }
 
   angular.module('cbitsPrototype.controllers')
